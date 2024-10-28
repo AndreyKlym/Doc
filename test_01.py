29853ca7cdd5aff55273ba0acf69d706b2e5231f
AndreyKlym <d3116280@gmail.com>
2024-10-28 00:34:07 -0400

    First Commit

---
+++ test_01.py
@@ -0,0 +1,30 @@
+a = 10
+b = 3
+c = 'abc'
+
+# print (a)
+# print (c)
+# print (a + b)
+
+# test = int(8/3)
+test = (8//3)
+# test = (10//3)
+# test = 4*6
+# test = 4-6
+# test = 1+2+3
+print (test)
+
+# import this
+
+
+
+
+
+
+# name = 'Jonh'
+# age = 32
+# likes_coffe = True
+
+# print(name)
+# print(age)
+# print(likes_coffe)
