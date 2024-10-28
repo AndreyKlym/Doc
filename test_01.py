25310561346b261d8f2099087a43fd731d6d0aa3
AndreyKlym <d3116280@gmail.com>
2024-10-28 01:36:09 -0400

    Symbols in the string

--- test_01.py
+++ test_01.py
@@ -1,49 +1,9 @@
-a = 10
-b = 3
-c = 'abc'
+# Индекс (порядковый номер символа) строки в Python
+# https://code.mu/ru/python/book/prime/basis/string-symbols/
 
-# print (a)
-# print (c)
-# print (a + b)
+txt = 'abcdefg'
 
-# test = int(8/3)
-# test = (8//3)
-# test = (10//3)
-# test = 4*6
-# test = 4-6
-# test = 1+2+3
-# print (test)
-
-# import this
-# num1 = 10 - 6 * 3 - 5
-num1 = -5
-num2 = 12
-
-# num1 = 200 ** 500
-# num2 = 3000
-
-# num1 = 67 ** 50
-# num2 = 45 ** 60
-
-num3 = num1 % num2
-# num1 -= 1
-print(num3)
-# print(10%3)
-
-txt1 = '12'
-txt2 = '+'
-txt3 = '5'
-txt4 = '17'
-
-print(txt1 + txt2 + txt3 + " = " + txt4)
-
-print (3 * txt1)
-
-txt5 = 'ab'
-txt6 = '123'
-
-# print (2 * txt5)
-print ((2 * txt5) + (2 * txt6))
+print (txt[2])
 
 
 
@@ -51,10 +11,13 @@ print ((2 * txt5) + (2 * txt6))
 
 
 
-# name = 'Jonh'
-# age = 32
-# likes_coffe = True
 
-# print(name)
-# print(age)
-# print(likes_coffe)
+
+
+
+
+
+
+
+
+
